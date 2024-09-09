53826afc0fd1e206882bba4cb73702c6c9e0bb1a
Francisco Farias <francisco@fticonsult.com.br>
2024-09-09 10:52:02 -0300

    Adicionado código para resolver questão 2 da aula 01.

--- logica_prog/lp_aula01.py
+++ logica_prog/lp_aula01.py
@@ -3,3 +3,12 @@ num1 = float(input("Digite um número: "))
 num2 = float(input("Digite outro número: "))
 soma = num1 + num2
 print(f"O resultado da soma é: {soma}.")
+
+# 2. Faça um programa que peça as 4 notas bimestrais e mostre a média.
+nota1 = float(input("Digite a nota do primeiro bimestre: "))
+nota2 = float(input("Digite a nota do segundo bimestre: "))
+nota3 = float(input("Digite a nota do terceiro bimestre: "))
+nota4 = float(input("Digite a nota do quarto bimestre: "))
+soma = nota1 + nota2 + nota3 + nota4
+media = soma / 4
+print(f"A média do aluno no ano, foi: {media}.")
