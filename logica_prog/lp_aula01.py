59b7cedc45df86d7d996e3346f50f7fed8b14f24
Francisco Farias <francisco@fticonsult.com.br>
2024-09-09 10:46:46 -0300

    Adicionado código para resolver questão 1 da aula 01.

---
+++ logica_prog/lp_aula01.py
@@ -0,0 +1,5 @@
+# 1. Faça um programa que peça dois números e imprima a soma.
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+soma = num1 + num2
+print(f"O resultado da soma é: {soma}.")
