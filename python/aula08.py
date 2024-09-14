08b8edfac3b3fe01cd9f6f75a8ec2e0155918f40
franciscofarias <francisco.fariasneto14@gmail.com>
2024-09-14 14:43:05 -0300

    Adicionado código para criar classes das atividades 1 e 2 da aula 08

---
+++ python/aula08.py
@@ -0,0 +1,13 @@
+class Cachorro:
+    def __init__(self, nome, idade, raca):
+        self.nome = nome
+        self.idade = idade
+        self.raca = raca
+
+class Pessoa:
+    def __init__(self, nome, idade, peso, genero):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.genero = genero
+
