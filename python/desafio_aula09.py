647e498482affc8754a630bfec130cca000c280a
franciscofariasneto <francisco.fariasneto14@gmail.com>
2024-09-22 09:55:40 -0300

    atualização de código projeto aula 09

--- python/desafio_aula09.py
+++ python/desafio_aula09.py
@@ -4,58 +4,94 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
     
-    def sacar(self):
-        if self.saldo >= 100:
-            self.saldo -= 100
+    def sacar(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
             print("Saque realizado com sucesso!")
         else:
             print("Saldo insuficiente!")
     
-    def depositar(self):
-        self.saldo += 100
+    def depositar(self, valor):
+        self.saldo += valor
         print("Deposito realizado com sucesso!")
     
     def saldo_atual(self):
         return self.saldo
     
+ftibank = []
+continuar = True
 
-contas = {}
+while continuar == True:
+    print("Bem-vindo(a) ao FTi Bank")
+    print("1 - Criar conta")
+    print("2 - Sacar dinheiro")
+    print("3 - Depositar dinheiro")
+    print("4 - Ver saldo")
+    print("8 - Listar Contas")
+    print("9 - Sair")
 
-print("Bem-vindo(a) ao FTi Bank")
-print("1 - Criar conta")
-print("2 - Sacar dinheiro")
-print("3 - Depositar dinheiro")
-print("4 - Ver saldo")
-main = input(": -> ")
-if main == "1":
-    numero = input("Digite o número da conta: ")
-    contas["Conta"] = numero
-    titular = input("Digite o nome do titular: ")
-    contas["Titular"] = titular
-    saldo = float(input("Digite o valor inicial da conta: "))
-    contas["Saldo"] = saldo
-elif main == "2":
-    numero = input("Digite o número da conta: ")
-    titular = input("Digite o nome do titular: ")
-    conta = Conta(numero, titular, input("Qual valor deseja sacar? "))
-elif main == "3":
-    numero = input("Digite o número da conta: ")
-    titular = input("Digite o nome do titular: ")
-    conta = Conta(numero, titular, input("Qual valor deseja depositar? "))
-elif main == "4":
-    numero = input("Digite o número da conta: ")
-    titular = input("Digite o nome do titular: ")
-    conta = Conta(numero, titular, input("Qual valor deseja verificar? "))
-else:
-    print("Opção inválida!")
+    main = input(": -> ")
 
-print(contas)
+    if main == "1":
+        numero = input("Digite o número da conta: ")
+        titular = input("Digite o nome do titular: ")
+        saldo = float(input("Digite o valor inicial da conta: "))
+        nova_conta = Conta(numero, titular, saldo)
+        ftibank.append(nova_conta)
+
+    elif main == "2":
+        numero = input("Digite o número da conta: ")
+        vlr_saque = float(input("Qual valor deseja sacar? "))
+        conta_encontrada = False
+
+        for conta in ftibank:
+            if conta.numero == numero:
+                conta.sacar(vlr_saque)
+                conta_encontrada = True
+                break
+
+        if not conta_encontrada:
+            print("Conta não encontrada!")
+
+    elif main == "3":
+        numero = input("Digite o número da conta: ")
+        vlr_deposito = float(input("Qual valor deseja depositar? "))
+        conta_encontrada = False
+
+        for conta in ftibank:
+            if conta.numero == numero:
+                conta.depositar(vlr_deposito)
+                conta_encontrada = True
+                break
+        
+        if not conta_encontrada:
+            print("Conta não encontrada!")
 
 
+    elif main == "4":
+        numero = input("Digite o número da conta: ")
+        conta_encontrada = False
 
+        for conta in ftibank:
+            if conta.numero == numero:
+                print(f"Saldo atual: {conta.saldo_atual()}")
+                conta_encontrada = True
+                break
 
-# cont = Conta(1234, "Francisco",  1000)
-# print(cont.saldo_atual()) # 1000
-# cont.sacar()
-# print(cont.saldo_atual()) # 900
+        if not conta_encontrada:
+            print("Conta não encontrada!")
+
+    elif main == "8":
+        for conta in ftibank:
+            print(f"Conta: {conta.numero} - Titular: {conta.titular} - Saldo: R$ {conta.saldo}")
+
+    elif main == "9":
+        continuar == False
+        print("Obrigado por usar o FTi Bank. Até logo!")
+        break
+
+    else:
+        print("Opção inválida!")
+
+ 
 
