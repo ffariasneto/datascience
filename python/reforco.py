7a84e8c674adb072cf479c6e238fd5c72bd7963d
Francisco Farias Neto <122189407+ffariasneto@users.noreply.github.com>
2024-10-25 18:14:46 -0300

    Reforço_rev2
    Atualizado arquivo da aula de reforço poo.

--- python/reforco.py
+++ python/reforco.py
@@ -19,6 +19,7 @@ class ApartamentoPadrao():
 class Studio(ApartamentoPadrao):
     def __init__(self, num_quartos, ambientes_sala, num_banheiros,
                  tem_area_servico, tem_varanda, numero, valor, cozinha_americana):
+        
         self.cozinha_americana = cozinha_americana
         
         super().__init__(num_quartos, ambientes_sala, num_banheiros,
@@ -36,11 +37,14 @@ class Studio(ApartamentoPadrao):
 lista = []
 while True:
     print("1 - Cadastrar Apartamento:")
-    print("2 - Listar Apartamentos:")
-    print("3 - Buscar valores por Apartamento:")
-    print("4 - Sair:")
+    print("2 - Cadastrar Studio: ")
+    print("3 - Listar Apartamentos:")
+    print("4 - Buscar valores por Apartamento:")
+    print("5 - Editar info Apartamento: ")
+    print("6 - Vender Imóvel:")
+    print("9 - Sair")
     op = input(": -> ")
-    if op == 1:
+    if op == 1 or op == 2:
         quartos = int(input("Digite o números de quartos: "))
         ambientes = int(input("Digite a qtde de ambientes da sala: "))
         banheiros = int(input("Digite o número de banheiros: "))
@@ -48,22 +52,47 @@ while True:
         varanda = int(input("Tem varanda? (S/N): "))
         numero = int(input("Digite o número do Apartamento: "))
         valor = int(input("Digite o valor do Apartamento: "))
-        apto = ApartamentoPadrao(quartos, ambientes, banheiros, 
-                           True if area == "S" else False,
-                           True if varanda == "S" else False,
-                           numero, valor)
+        if op == 1:
+            apto = ApartamentoPadrao(quartos, ambientes, banheiros, 
+                            True if area == "S" else False,
+                            True if varanda == "S" else False,
+                            numero, valor)
+        if op == 2:
+            cozinha = input("Tem cozinha americana? (S/N): ")
+            apto = Studio(quartos, ambientes, banheiros, 
+                            True if area == "S" else False,
+                            True if varanda == "S" else False,
+                            numero, valor, True if cozinha == "S" else False)
         lista.append(apto)
 
-    if op == 2:
-        for apto in lista:
-            print(f"O valor do apto {apto.numero} é R$ {apto.valor:.2f}.")
-
     if op == 3:
+        for apto in lista:
+            if apto.disponibilidade == "Disponível":
+                print(f"O valor do apto {apto.numero} é R$ {apto.valor:.2f}.")
+
+    if op == 4:
         apto_busca = int(input("Digite o número do apartamento: "))
         for apto in lista:
             if apto.numero == apto_busca:
                 print(f"O valor do apto {apto.numero} é R$ {apto.valor:.2f}.")
+    
+    if op == 5:
+        apto_busca = int(input("Digite o número do apartamento: "))
+        apto_encontrado = False
+        for apto in lista:
+            if apto.numero == apto_busca:
+                novo_valor = float(input("Digite o novo valor: "))
+                apto.valor = novo_valor
+                apto_encontrado = True
+        if not apto_encontrado:
+            print("Apartamento não encontrado!")
+    
+    if op == 6:
+        apto_busca = int(input("Digite o número do apartamento: "))
+        for apto in lista:
+            if apto.numero == apto_busca:
+                apto.disponibilidade = "Indisponível"
 
-    if op == 4:
+    if op == 9:
         print("Saindo do programa...")
         break
